78069a7bbf47ade10c7bd2478da2067f15d7da01
lobotelho22 <eduardo.ocara@gmail.com>
2022-07-14 21:41:20 -0300

    requisitos 3 e 4: testes ok, cobertura de testes ok

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,18 +1,67 @@
-import React from 'react';
+import React, { useState } from 'react';
 
+const INITIAL_USER_DATA = {
+  email: '',
+  password: '',
+};
 function Login() {
+  const [userData, setUserData] = useState(INITIAL_USER_DATA);
+  const { email, password } = userData;
+
+  const handleChange = ({ target: { name, value } }) => {
+    setUserData({
+      ...userData,
+      [name]: value,
+    });
+  };
+
+  const handleClick = () => {
+    console.log('é apenas um teste!');
+  };
+
+  const verifyConditions = () => {
+    const emailRegex = /^[\w+.]+@\w+\.\w{2,}(?:\.\w{2})?$/; // O Regex é cortesia de: https://pt.stackoverflow.com/questions/1386/express%C3%A3o-regular-para-valida%C3%A7%C3%A3o-de-e-mail
+    const testEmail = emailRegex.test(email);
+    const minLength = 6;
+    const testPwd = password.length > minLength;
+    return !(testEmail && testPwd);
+  };
+
   return (
-    <main>
+    <form className="loginItens">
       <label htmlFor="email">
         E-mail:
-        <input type="text" id="email" data-testid="email-input" />
+        {' '}
+        <input
+          type="text"
+          id="email"
+          name="email"
+          data-testid="email-input"
+          onChange={ handleChange }
+          value={ email }
+        />
       </label>
       <label htmlFor="password">
         Password:
-        <input type="password" id="password" data-testid="password-input" />
+        {' '}
+        <input
+          type="password"
+          id="password"
+          name="password"
+          data-testid="password-input"
+          onChange={ handleChange }
+          value={ password }
+        />
       </label>
-      <button type="button" data-testid="login-submit-btn">Enter</button>
-    </main>
+      <button
+        type="button"
+        data-testid="login-submit-btn"
+        disabled={ verifyConditions() }
+        onClick={ handleClick }
+      >
+        Enter
+      </button>
+    </form>
   );
 }
 
--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
 
 describe('Testes da Tela de Login', () => {
-  test('01. Os campos de email e senha, além do botão "Enter" são renderizados', () => {
+  beforeEach(() => {
     render(<App />);
+  })
+  test('01. Os campos de email e senha, além do botão "Enter" são renderizados', () => {
     const inputEmail = screen.getByRole('textbox', { name: /e-mail/i });
     const inputPwd = screen.getByLabelText(/password/i);
     const buttonEnter = screen.getByRole('button', { name: /enter/i });
@@ -12,6 +15,39 @@ describe('Testes da Tela de Login', () => {
     expect(inputEmail).toBeInTheDocument();
     expect(inputPwd).toBeInTheDocument();
     expect(buttonEnter).toBeInTheDocument();
+  });
+
+  test('02. Verifica se o botão só é habilitado após email e senha válidos', () => {
+    const inputEmail = screen.getByRole('textbox', { name: /e-mail/i });
+    const inputPwd = screen.getByLabelText(/password/i);
+    const buttonEnter = screen.getByRole('button', { name: /enter/i });
+
+    expect(buttonEnter).toBeDisabled();
+
+    userEvent.type(inputEmail, 'juma_marrua');
+    userEvent.type(inputPwd, '123456');
+
+    expect(buttonEnter).toBeDisabled();
+
+    userEvent.clear(inputEmail);
+    userEvent.clear(inputPwd);
+
+    userEvent.type(inputEmail, 'juma_marrua@pantanal.com.br');
+    userEvent.type(inputPwd, '1234567');
+
+    expect(buttonEnter).toBeEnabled();
+
+  });
+
+  test('03. Verifica se é possível clicar no botão', () => {
+    const inputEmail = screen.getByRole('textbox', { name: /e-mail/i });
+    const inputPwd = screen.getByLabelText(/password/i);
+    const buttonEnter = screen.getByRole('button', { name: /enter/i });
+
+    userEvent.type(inputEmail, 'juma_marrua@pantanal.com.br');
+    userEvent.type(inputPwd, '1234567');
+    userEvent.click(buttonEnter);
+
   });
 
 })
