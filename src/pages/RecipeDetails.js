508419e93d48ea3b7d2aa82e25d8072588b81dac
lobotelho22 <eduardo.ocara@gmail.com>
2022-07-20 19:03:54 -0300

    tela de recipe in progress: renderizando todos os elementos

--- src/pages/RecipeDetails.js
+++ src/pages/RecipeDetails.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import ReceitasContext from '../hooks/ReceitasContext';
 import useActualPath from '../hooks/useActualPath';
 import useDetails from '../hooks/useDetails';
 
 function RecipeDetails() {
   const actualpath = useActualPath();
+
   useDetails(actualpath);
+
+  const { recipesData: { recipeInDetail } } = useContext(ReceitasContext);
+  const objLength = Object.keys(recipeInDetail).length;
+  console.log(recipeInDetail);
 
   return (
     <main>
-      <h1>
-        { actualpath }
-      </h1>
+      <div>
+        {
+          (objLength)
+            ? <h1>teste1</h1>
+            : <h1>loading...</h1>
+        }
+      </div>
     </main>
   );
 }
--- src/pages/RecipeInProgress.js
+++ src/pages/RecipeInProgress.js
@@ -1,16 +1,105 @@
-import React from 'react';
-import underConstrucion from '../images/under_construction.svg';
+import React, { useContext } from 'react';
+import ReceitasContext from '../hooks/ReceitasContext';
+import useActualPath from '../hooks/useActualPath';
+import useDetails from '../hooks/useDetails';
 
 function RecipeInProgress() {
+  const actualPath = useActualPath();
+  useDetails(actualPath);
+  const { recipesData: { recipeInDetail } } = useContext(ReceitasContext);
+
+  const getIngredientsList = () => {
+    const allKeys = Object
+      .keys(recipeInDetail)
+      .filter((item) => item.includes('strIngredient'))
+      .map((item) => recipeInDetail[item])
+      .filter((item) => item !== '' && item !== null);
+    return allKeys;
+  };
+
+  const getIngredients = () => {
+    const ingredientsList = getIngredientsList();
+    return ingredientsList.map((ingredient, index) => (
+      <li data-testid={ `${index}-ingredient-step` } key={ `${ingredient}_${index}` }>
+        <input type="checkbox" />
+        {ingredient}
+      </li>
+    ));
+  };
+
+  const getProgressInfo = () => {
+    if (actualPath.includes('foods')) {
+      return (
+        <>
+          <img
+            data-testid="recipe-photo"
+            className="imgInProgress"
+            src={ recipeInDetail.strMealThumb }
+            alt={ `${recipeInDetail.strMeal}` }
+          />
+          <h1 data-testid="recipe-title">
+            {recipeInDetail.strMeal}
+          </h1>
+          <button type="button" data-testid="share-btn">
+            Compartilhar
+          </button>
+          <button type="button" data-testid="favorite-btn">
+            Favoritar
+          </button>
+          <h4 data-testid="recipe-category">
+            Categoria:
+            {' '}
+            {recipeInDetail.strCategory}
+          </h4>
+          <ul>
+            { getIngredients() }
+          </ul>
+          <p data-testid="instructions">{recipeInDetail.strInstructions}</p>
+          <button type="button" data-testid="finish-recipe-btn">
+            Finalizar
+          </button>
+        </>
+      );
+    }
+
+    if (actualPath.includes('drinks')) {
+      return (
+        <>
+          <img
+            data-testid="recipe-photo"
+            className="imgInProgress"
+            src={ recipeInDetail.strDrinkThumb }
+            alt={ `${recipeInDetail.strDrink}` }
+          />
+          <h1 data-testid="recipe-title">
+            {recipeInDetail.strDrink}
+          </h1>
+          <button type="button" data-testid="share-btn">
+            Compartilhar
+          </button>
+          <button type="button" data-testid="favorite-btn">
+            Favoritar
+          </button>
+          <h4 data-testid="recipe-category">
+            Categoria:
+            {' '}
+            {recipeInDetail.strAlcoholic}
+          </h4>
+          <ul>
+            { getIngredients() }
+          </ul>
+          <p data-testid="instructions">{recipeInDetail.strInstructions}</p>
+          <button type="button" data-testid="finish-recipe-btn">
+            Finalizar
+          </button>
+        </>
+      );
+    }
+  };
+
   return (
     <main>
-      <img
-        src={ underConstrucion }
-        alt="site em construção..."
-        className="under_construction"
-      />
-      <h4>mulheres e homens trabalhando!</h4>
-      <p>E trabalhando demais...</p>
+      { getProgressInfo() }
     </main>
   );
 }
